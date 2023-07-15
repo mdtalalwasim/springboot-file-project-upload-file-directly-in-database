df2aa29d97bef8e88c6a82f5fe050b5e707c0afd
Md. Talal Wasim <md.talalwasim@gmail.com>
2023-07-15 18:09:14 +0600

    Project setup and initial commit.

---
+++ springboot-file-project-upload-file-directly-in-database/src/main/java/com/mdtalalwasim/fileproject/SpringbootFileProjectUploadFileDirectlyInDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.fileproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileProjectUploadFileDirectlyInDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFileProjectUploadFileDirectlyInDatabaseApplication.class, args);
+	}
+
+}
---
+++ springboot-file-project-upload-file-directly-in-database/src/test/java/com/mdtalalwasim/fileproject/SpringbootFileProjectUploadFileDirectlyInDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdtalalwasim.fileproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFileProjectUploadFileDirectlyInDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
